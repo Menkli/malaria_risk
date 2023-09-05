fe5a8909ef79823ffbf981f6bbdc1dca02568fc4
Menkli <Linda.Menk@stud.sbg.ac.at>
2023-09-05 09:23:35 +0200

    update

--- 00_acled_credentials.R
+++ 00_acled_credentials.R
@@ -1,3 +1,3 @@
 # This needs to go into a separate file!
-Sys.setenv(EMAIL_ADDRESS = "Linda.Menk@plus.ac.at")
-Sys.setenv(ACCESS_KEY = "Acd6pUnN0Karux-DAfrh")
+Sys.setenv(EMAIL_ADDRESS = "place email address here")
+Sys.setenv(ACCESS_KEY = "place access key here")
--- 00_copernicus_credentials.R
+++ 00_copernicus_credentials.R
@@ -1,5 +1,3 @@
-# User ID 36664
-# API Key 811ce88a-f8ed-4f81-8f5a-2c56956895fa
-wf_set_key(user = "36664",
-           key = "811ce88a-f8ed-4f81-8f5a-2c56956895fa",
+wf_set_key(user = "place user ID here",
+           key = "place access key here",
            service = "cds")
